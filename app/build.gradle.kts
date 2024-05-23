928fe8152d6ad8c7863318c1f1c80a514a9d970f
ivan_khomiak <ivan_khomiak@epam.com>
2024-05-23 18:40:13 +0300

    set up AndroidApplicationConventionPlugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,12 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.runique.android.application)
     alias(libs.plugins.mapsplatform.secrets.plugin)
 }
 
 android {
     namespace = "com.scientisthamster.runique"
-    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.scientisthamster.runique"
-        minSdk = 29
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -30,13 +22,7 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+
     buildFeatures {
         compose = true
     }
